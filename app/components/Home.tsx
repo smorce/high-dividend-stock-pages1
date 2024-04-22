6f843705716336cc56b51aef4533367fc9d80348
smorce <smorcepie@gmail.com>
2024-04-22 14:24:16 +0900

    ヘッダー修正

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -13,7 +13,7 @@
 // client コンポーネント
 "use client";  // この行を追加
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import './tableStyles.css';
 
@@ -62,7 +62,7 @@ interface HomeProps {
 // export default async function Home({ data }: HomeProps) {
 export default function Home({ data }: HomeProps) {
   const [isDataExpanded, setIsDataExpanded] = useState(false);
-  const [isHeaderFixed, setIsHeaderFixed] = useState(false);
+  const tableContainerRef = useRef<HTMLDivElement>(null); // テーブルコンテナの参照を初期化
 
   const toggleData = () => {
     setIsDataExpanded(!isDataExpanded);
@@ -71,10 +71,19 @@ export default function Home({ data }: HomeProps) {
   useEffect(() => {
     const handleScroll = () => {
         const currentScrollPosition = window.scrollY;
-        const scrollThreshold = 450; // スクロール閾値を500pxに設定
+        const scrollThreshold = 500; // スクロール閾値を500pxに設定
 
-        // スクロール位置が閾値を超えた場合に状態を更新
-        setIsHeaderFixed(currentScrollPosition > scrollThreshold);
+        if (currentScrollPosition > scrollThreshold) {
+            if (tableContainerRef.current) { // null チェックを追加
+                tableContainerRef.current.style.maxHeight = '800px';
+                tableContainerRef.current.style.overflowY = 'auto';
+            }
+        } else {
+            if (tableContainerRef.current) { // null チェックを追加
+                tableContainerRef.current.style.maxHeight = 'none';
+                tableContainerRef.current.style.overflowY = 'hidden';
+            }
+        }
     };
 
     window.addEventListener('scroll', handleScroll);
@@ -83,6 +92,7 @@ export default function Home({ data }: HomeProps) {
         window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+
 
   return (
     <>
@@ -129,15 +139,7 @@ export default function Home({ data }: HomeProps) {
             </span>
           </div>
         </div>
-
-        <div
-            className="table-container mt-6 table-shadow"
-            style={{
-                maxHeight: isHeaderFixed ? '800px' : 'none',
-                overflowY: isHeaderFixed ? 'auto' : 'hidden'
-            }}
-        >
-
+        <div ref={tableContainerRef} className="table-container mt-6 table-shadow">
           <table className="text-sm text-left text-gray-500" id="data-table">
             <thead className="table-header text-xs text-gray-700 uppercase">
               <tr>
