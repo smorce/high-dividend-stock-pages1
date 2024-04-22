5955c17a83a72639828e37eb3fa757d6f2f54102
smorce <smorcepie@gmail.com>
2024-04-22 11:22:03 +0900

    ヘッダー修正

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -13,7 +13,7 @@
 // client コンポーネント
 "use client";  // この行を追加
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import './tableStyles.css';
 
@@ -62,10 +62,22 @@ interface HomeProps {
 // export default async function Home({ data }: HomeProps) {
 export default function Home({ data }: HomeProps) {
   const [isDataExpanded, setIsDataExpanded] = useState(false);
+  const [isHeaderFixed, setIsHeaderFixed] = useState(false);
 
   const toggleData = () => {
     setIsDataExpanded(!isDataExpanded);
   };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsHeaderFixed(window.scrollY > 100);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <>
@@ -113,9 +125,9 @@ export default function Home({ data }: HomeProps) {
           </div>
         </div>
 
-        <div className="table-container mt-6 table-shadow">
+        <div className={`table-container mt-6 table-shadow ${isHeaderFixed ? 'table-scroll' : ''}`}>
           <table className="text-sm text-left text-gray-500" id="data-table">
-            <thead className="table-header text-xs text-gray-700 uppercase">
+            <thead className={`table-header text-xs text-gray-700 uppercase ${isHeaderFixed ? 'sticky-header' : ''}`}>
               <tr>
                 <th scope="col" className="px-6 py-3 width-70">ティッカー</th>
                 <th scope="col" className="px-6 py-3 width-100">企業名</th>
